d89c8f37b3b36f3f0c6c0af0e65ccddd640965e4
Rohit Ray <59976277+rohitray042@users.noreply.github.com>
2020-08-03 14:40:05 +0530

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,29 @@
+/*Name this external file gallery.js*/
+
+function upDate(previewPic){
+    var image = document.getElementById('image');
+    image.innerHTML= previewPic.alt;
+    image.style.background ="url('"+ previewPic.src +"')";
+  
+ /* In this function you should 
+    1) change the url for the background image of the div with the id = "image" 
+    to the source file of the preview image
+    
+    2) Change the text  of the div with the id = "image" 
+    to the alt text of the preview image 
+    */
+  
+	}
+function unDo(){
+    var image = document.getElementById('image');
+    image.style.background = "url('') #8e68ff";
+    image.innerHTML ="Hover over an image below to display here.";
+     /* In this function you should 
+    1) Update the url for the background image of the div with the id = "image" 
+    back to the orginal-image.  You can use the css code to see what that original URL was
+    
+    2) Change the text  of the div with the id = "image" 
+    back to the original text.  You can use the html code to see what that original text was
+    */
+		
+	}
